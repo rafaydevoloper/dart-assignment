d3c6975dde1925b93aa7c5eed1f07006aa95e617
Abdul Rafay <aslamabdulrafay83@gmail.com>
2026-09-01 16:25:34 +0500

    Update project

--- main.dart
+++ main.dart
@@ -1,4 +1,26 @@
-void main(){
+void main () {
 
-  print("Hello World");
+Map<String,String> student = {
+  "name": "rafay",
+  "city": "karachi",
+  "age": "18",
+};
+ print (student);
+
+print(student["name"]);
+
+print (student.remove("age"));
+print (student.containsKey("name"));
+print (student.containsKey("country"));
+print (student.containsValue("18"));
+
+print (student.length);
+
+print (student.keys);
+
 }
+
+
+
+ Map<String, String> newMethod(Map<String, String> student) => student; 
+
